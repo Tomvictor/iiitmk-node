5f4a976096d3e4b615d3d7b2a19ba79324f69608
tomvictor <vjtomvictor@icloud.com>
2018-04-19 23:22:34 +0530

    bul added

--- iiitmk.js
+++ iiitmk.js
@@ -4,7 +4,6 @@ var request = require('request');
 const URL = "http://139.59.29.99"
 
 
-
 // {
 // "latitude" : "123" ,
 // "longitude" : "123" ,
@@ -12,80 +11,82 @@ const URL = "http://139.59.29.99"
 // }
 
 
-var server = net.createServer(function(socket) {
-	//listening event is not working
-	socket.on('listening',() => {
-		console.log('listening for connections');
-	    });
-	console.log("client connected"); //message on new connection
-	console.log(socket.address()); //This will display the client address
-	socket.write('$ready'); //imediate message to client
-	//tring to read the data
-	socket.on('data',(data) => {
-		//console.log("Data Recieved:");
-		//console.log(data);
-		//console.log("Data length:");
-		//console.log(data.length);
-		//console.log("Data in ascii");
-		var dataString = data.toString('ascii');
-		console.log("recieved data . . . ")
-		console.log(dataString);
-		//returning acknowledge
+var server = net.createServer(function (socket) {
+    //listening event is not working
+    socket.on('listening', () => {
+        console.log('listening for connections');
+    });
+    console.log("client connected"); //message on new connection
+    console.log(socket.address()); //This will display the client address
+    socket.write('$ready'); //imediate message to client
+    //tring to read the data
+    socket.on('data', (data) => {
+        //console.log("Data Recieved:");
+        //console.log(data);
+        //console.log("Data length:");
+        //console.log(data.length);
+        //console.log("Data in ascii");
+        var dataString = data.toString('ascii');
+        console.log("recieved data . . . ")
+        console.log(dataString);
+        //returning acknowledge
+        var data;
 
-		try{
-			jsonObj = JSON.parse(dataString);
+        try {
+            jsonObj = JSON.parse(dataString);
 
-		console.log(jsonObj.latitude);
+            if (jsonObj.switch) {
+                data = {
+                    "latitude": jsonObj.latitude,
+                    "longitude": jsonObj.longitude,
+                    "device": jsonObj.deviceid
+                }
+            }
 
-		}catch(err){
+            console.log("sending api request...");
 
-			console.log("error on json parsing") ;
-			console.log(err) ;
+            var options = {
+                uri: URL + '/api/?format=json',
+                method: 'POST',
+                json: data
+            };
 
-		}
-		
 
-		var data = {
-          "latitude" :  dataString,
-          "longitude" : dataString ,
-          "device" : 1
+            console.log(options);
+
+            request(options, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body) // Print the shortened url.                                                                                                                           
+                }
+            });
+
+            console.log(jsonObj.latitude);
+
+
+        } catch (err) {
+
+            console.log("error on json parsing");
+            console.log(err);
+
         }
 
-  		console.log("sending api request...") ;
 
-		var options = {
-		        uri: URL + '/api/?format=json',
-		        method: 'POST',
-		        json: data
-		    };
+        var flag;
+        flag = socket.write("ok");
+        //console.log(flag);
 
-
-		 console.log(options);
-
-		request(options, function (error, response, body) {
-		  if (!error && response.statusCode == 200) {
-		    console.log(body) // Print the shortened url.                                                                                                                           
-		  }
-		});
-
-
-
-		var flag; 
-		flag = socket.write("ok") ;
-		//console.log(flag);
-		
-	    });
-	socket.on('error',(err) => {
-		console.log("Error occured");
-		console.log(err);
-	    });
-
-	//socket.pipe(socket);
-
-	//message on disconnecting client
-	socket.on('end', () => {
-		console.log('client disconnected');
-	    });
     });
+    socket.on('error', (err) => {
+        console.log("Error occured");
+        console.log(err);
+    });
+
+    //socket.pipe(socket);
+
+    //message on disconnecting client
+    socket.on('end', () => {
+        console.log('client disconnected');
+    });
+});
 
 server.listen(8800, '0.0');
